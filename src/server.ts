0f7814d9265d00479e2b3f11fc12895374f80e96
md-musa <mohammad.musa706@gmail.com>
2025-01-23 16:27:44 +0600

    handled errors and logs

--- src/server.ts
+++ src/server.ts
@@ -2,47 +2,47 @@ import { Server } from "http";
 import app from "./app";
 import mongoose from "mongoose";
 import config from "./config";
-// import { errorlogger, logger } from "./shared/logger";
+import { logger } from "./shared/logger";
 
 let server: Server;
 
 const main = async () => {
   try {
     await mongoose.connect(config.database_url as string);
-    console.log("✅ Database connected successfully");
+    logger.info("✅ Database connected successfully");
 
     server = app.listen(config.port, () => {
-      console.log(`🚀 Server running on port ${config.port}`);
+      logger.info(`🚀 Server running on port ${config.port}`);
     });
   } catch (error) {
-    console.log("❌ Database connection failed", error);
+    logger.error("❌ Database connection failed", error);
     process.exit(1);
   }
+
+  const shutdownServer = (reason: string) => {
+    console.log(`⚠ Shutting down due to ${reason}`);
+
+    if (server) {
+      server.close(() => {
+        console.log("🔴 Server closed");
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  };
+
+  process.on("uncaughtException", (error) => {
+    console.log("❗ Uncaught Exception:", error);
+    shutdownServer("uncaughtException");
+  });
+
+  process.on("unhandledRejection", (reason) => {
+    console.log("❗ Unhandled Rejection:", reason);
+    shutdownServer("unhandledRejection");
+  });
+
+  process.on("SIGTERM", () => shutdownServer("SIGTERM"));
 };
 
 main();
-
-
-  // const exitHandler = (reason: string) => {
-  //   console.log(`⚠ Shutting down due to ${reason}`);
-  //   if (server) {
-  //     server.close(() => {
-  //       console.log("🔴 Server closed");
-  //       process.exit(1);
-  //     });
-  //   } else {
-  //     process.exit(1);
-  //   }
-  // };
-
-  // process.on("uncaughtException", (error) => {
-  //   console.log("❗ Uncaught Exception:", error);
-  //   exitHandler("uncaughtException");
-  // });
-
-  // process.on("unhandledRejection", (reason) => {
-  //   console.log("❗ Unhandled Rejection:", reason);
-  //   exitHandler("unhandledRejection");
-  // });
-
-  // process.on("SIGTERM", () => exitHandler("SIGTERM"));
